f165837ea7a2f56ebbf906a8966c538d812ef442
UCIX0 <javier.uc@outlook.com>
2023-05-25 03:32:39 -0600

    103 tree2

--- 102-binary_tree_is_complete.c
+++ 102-binary_tree_is_complete.c
@@ -7,7 +7,7 @@ int binary_tree_is_complete(const binary_tree_t *tree);
  * binary_tree_height - Measures the height of a binary tree
  * @tree: pointer to the root node of the tree to measure the height
  *
- * Return: height of the tree, or 0 if tree is NULL
+ * Return: height of the tree, or 0 if tree is NUL
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
